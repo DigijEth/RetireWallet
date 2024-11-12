786b177ca7be5cc8f5be958f3ec4cc853f67e209
Digi J <fightwithmusicinc@gmail.com>
2024-11-11 19:05:52 -0800

    Create ApiService.kt

---
+++ app/src/main/java/com/default/retirementbasewallet/api/ApiService.kt
@@ -0,0 +1,10 @@
+package com.default.retirementbasewallet.api
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface ApiService {
+    @GET("/api/hardship/{address}")
+    fun getHardshipCodes(@Path("address") address: String): Call<List<String>>
+}
