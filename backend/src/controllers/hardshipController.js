cd4de26cad11b6f97996dfb6e14d9b2123182634
Digi J <fightwithmusicinc@gmail.com>
2024-11-11 18:35:58 -0800

    Update hardshipController.js
    error handling

--- backend/src/controllers/hardshipController.js
+++ backend/src/controllers/hardshipController.js
@@ -6,25 +6,36 @@ const generateHardshipCodes = () => {
   return Array.from({ length: 3 }, () => crypto.randomBytes(4).toString("hex"));
 };
 
-const getHardshipCodes = async (req, res) => {
-  const { address } = req.params;
-  const user = await User.findOne({ address });
-  if (!user) return res.status(404).json({ message: "User not found" });
-
-  res.json({ hardshipCodes: user.hardshipCodes });
+const getHardshipCodes = async (req, res, next) => {
+  try {
+    const { address } = req.params;
+    const user = await User.findOne({ address });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json({ hardshipCodes: user.hardshipCodes });
+  } catch (err) {
+    console.error("Error fetching hardship codes:", err);
+    next(err); // Pass the error to the global error handler
+  }
 };
 
-const createHardshipCodes = async (req, res) => {
-  const { address } = req.body;
-  let user = await User.findOne({ address });
-  if (!user) {
-    user = new User({ address });
+const createHardshipCodes = async (req, res, next) => {
+  try {
+    const { address } = req.body;
+    let user = await User.findOne({ address });
+    if (!user) {
+      user = new User({ address });
+    }
+
+    user.hardshipCodes = generateHardshipCodes();
+    await user.save();
+
+    res.json({ hardshipCodes: user.hardshipCodes });
+  } catch (err) {
+    console.error("Error creating hardship codes:", err);
+    next(err); // Pass the error to the global error handler
   }
-
-  user.hardshipCodes = generateHardshipCodes();
-  await user.save();
-
-  res.json({ hardshipCodes: user.hardshipCodes });
 };
 
 module.exports = {
