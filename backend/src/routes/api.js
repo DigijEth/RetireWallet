e767cfe2d3bbc35755f54c5757eb1fcb3d9fb8e5
Digi J <fightwithmusicinc@gmail.com>
2024-11-11 18:38:15 -0800

    Update api.js
    error handling

--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -3,7 +3,20 @@ const express = require("express");
 const { getHardshipCodes, createHardshipCodes } = require("../controllers/hardshipController");
 const router = express.Router();
 
-router.get("/hardship/:address", getHardshipCodes);
-router.post("/hardship", createHardshipCodes);
+router.get("/hardship/:address", async (req, res, next) => {
+  try {
+    await getHardshipCodes(req, res);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/hardship", async (req, res, next) => {
+  try {
+    await createHardshipCodes(req, res);
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
